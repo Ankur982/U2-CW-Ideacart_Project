b58be307c75b17af9bcf22ecede885c95d6cdaf3
Ankur982 <singh999ankur@gmail.com>
2022-06-18 19:19:08 +0530

    updaiting

--- signin.js
+++ signin.js
@@ -1,30 +1,31 @@
-<script>
-    let users=JSON.parse(localStorage.getItem("userData"))||[];
-    document.querySelector("button").addEventListener("click",mySignIn);
-    function mySignIn(event){
-        event.preventDefault();
-        let data={
-           userEmail:document.querySelector("#entered_email").value,
-           userPassword:document.querySelector("#entered_password").value
-        }
-           
-        if(checkSignin(data.userEmail,data.userPassword)==true){
-            localStorage.setItem("signinData",JSON.stringify(data));
-            alert("sign in successful")
-            window.location.href="#";
-        }else{
-            alert("worng email or password")
-        }
+let users = JSON.parse(localStorage.getItem("userData")) || [];
+document.querySelector("#signin").addEventListener("click", mySignIn);
 
-       function checkSignin(email,password){
-        let filter=users.filter(function(ele){
-            return ele.email==email && ele.password==password
-        })
-        if(filter.length>0){
-            return true;
-        }else{
-            return false
-        }
-       }
+function mySignIn(event) {
+  event.preventDefault();
+  let data = {
+    userEmail: document.querySelector("#entered_email").value,
+    userPassword: document.querySelector("#entered_password").value,
+  }
+
+  if (data.userEmail == "admin@gmail.com" && data.userPassword == "admin") {
+    window.location.href = "https://www.google.com";
+  } else if (checkSignin(data.userEmail, data.userPassword) == true) {
+    alert("sign in successful")
+    window.location.href = "home_page.html";
+    localStorage.setItem("signinData", JSON.stringify(data));
+  } else {
+    alert("Invalid email or password")
+  }
+
+  function checkSignin(email, password) {
+    let filter = users.filter(function(ele) {
+      return ele.email == email && ele.password == password
+    })
+    if (filter.length > 0) {
+      return true;
+    } else {
+      return false
     }
-</script>
+  }
+}
--- signup.js
+++ signup.js
@@ -1,38 +1,37 @@
-<script>
-    document.querySelector("#signup").addEventListener("click",mySignup);
-    let dataArr=JSON.parse(localStorage.getItem("userData"))||[];
-    function mySignup(event){
-        event.preventDefault();
-        let dataObj={
-            name:document.querySelector("#name").value,
-            mobile:document.querySelector("#mobile").value,
-            email:document.querySelector("#email").value,
-            password:document.querySelector("#password").value
-        };
+document.querySelector("#signup").addEventListener("click", mySignup);
+let dataArr = JSON.parse(localStorage.getItem("userData")) || [];
 
-        if(checkEmail(dataObj.email)==true){
-            dataArr.push(dataObj);
-        localStorage.setItem("userData",JSON.stringify(dataArr));
-        if(dataObj.email==""||dataObj.password==""||dataObj.name==""){
-            alert("please fill all details")
-        }else{
-            alert("Sign up completed....please signin");
-            window.location.href="#";
-        }
-        }else{
-            alert("Account already exist")
-        }
+function mySignup(event) {
+  event.preventDefault();
+  let dataObj = {
+    name: document.querySelector("#name").value,
+    mobile: document.querySelector("#mobile").value,
+    email: document.querySelector("#email").value,
+    password: document.querySelector("#password").value
+  };
 
-        function checkEmail(email){
-            let filtered=dataArr.filter(function(ele){
-                return email===ele.email;
-            })
-            if(filtered.length>0){
-                return false;
-            }else{
-                return true;
-            }
-
-        }
+  if (checkEmail(dataObj.email) == true) {
+    dataArr.push(dataObj);
+    localStorage.setItem("userData", JSON.stringify(dataArr));
+    if (dataObj.email == "" || dataObj.password == "" || dataObj.name == "") {
+      alert("please fill all details")
+    } else {
+      alert("Sign up completed....please signin");
+        document.querySelector('.cont').classList.toggle('s--signup');
     }
-</script>
+  } else {
+    alert("Account already exist")
+  }
+
+  function checkEmail(email) {
+    let filtered = dataArr.filter(function(ele) {
+      return email === ele.email;
+    })
+    if (filtered.length > 0) {
+      return false;
+    } else {
+      return true;
+    }
+
+  }
+}

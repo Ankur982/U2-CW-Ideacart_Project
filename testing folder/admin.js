dac3b1de6c1dc9c47c6d75f20769ae1b2423ba6b
priyanshupawar <talk20priyanshu@gmail.com>
2022-06-19 02:39:17 +0530

    complete Dashboard work

---
+++ testing folder/admin.js
@@ -0,0 +1,96 @@
+let uploadedArr = JSON.parse(localStorage.getItem("New-Product")) || [];
+let review = JSON.parse(localStorage.getItem("review"));
+
+function displayProducts(uploadedArr) {
+  document.querySelector("#tbody").innerHTML = "";
+
+  uploadedArr.map(function (el) {
+    let tr = document.createElement("tr");
+    let td1 = document.createElement("td");
+    let image = document.createElement("img");
+    image.src = el.image;
+    td1.append(image);
+
+    let name = document.createElement("td");
+    name.innerText = el.description;
+
+    let price = document.createElement("td");
+    price.innerText = el.price;
+
+    let accept = document.createElement("td");
+    accept.innerText = "Accept";
+
+    accept.addEventListener("click", function () {
+      acceptFun(el);
+    });
+
+    let reject = document.createElement("td");
+    reject.innerText = " Reject";
+
+    reject.addEventListener("click", function () {
+      rejectFun(el);
+    });
+
+    tr.append(td1, name, price, accept, reject);
+
+    document.querySelector("#tbody").append(tr);
+  });
+}
+
+displayProducts(uploadedArr);
+
+let acceptedItems = 0;
+function acceptFun(el) {
+  let productLS = [];
+  let obj = {
+    description: el.description,
+    image: el.image,
+    price: el.price,
+    type: el.type,
+    id: el.id,
+  };
+
+  acceptedItems++;
+  review--;
+  productLS.push(obj);
+  localStorage.setItem("accepted-Product", JSON.stringify(productLS));
+  localStorage.setItem("review", JSON.stringify(review));
+  localStorage.setItem("aproved", JSON.stringify(acceptedItems));
+
+  //deleting item from LS
+
+  let deleted = uploadedArr.filter(function (element) {
+    return element.id != el.id;
+  });
+  uploadedArr = deleted;
+  localStorage.setItem("New-Product", JSON.stringify(uploadedArr));
+  displayProducts(uploadedArr);
+}
+
+let rejectedItems = 0;
+function rejectFun(el) {
+  let productLS = [];
+  let obj = {
+    description: el.description,
+    image: el.image,
+    price: el.price,
+    type: el.type,
+    id: el.id,
+  };
+
+  rejectedItems++;
+  review--;
+  productLS.push(obj);
+  localStorage.setItem("rejected-Product", JSON.stringify(productLS));
+  localStorage.setItem("review", JSON.stringify(review));
+  localStorage.setItem("reject", JSON.stringify(rejectedItems));
+
+  //deleting item from LS
+
+  let deleted = uploadedArr.filter(function (element) {
+    return element.id != el.id;
+  });
+  uploadedArr = deleted;
+  localStorage.setItem("New-Product", JSON.stringify(uploadedArr));
+  displayProducts(uploadedArr);
+}
---
+++ testing folder/upload_product.js
@@ -0,0 +1,49 @@
+let reviewItems = JSON.parse(localStorage.getItem("review")) || 0;
+let productLS = JSON.parse(localStorage.getItem("New-Product")) || [];
+JSON.parse(localStorage.getItem("added-Product")) || [];
+document.querySelector("#form").addEventListener("submit", addPoductFun);
+let formData = document.querySelector("#form");
+
+function addPoductFun() {
+  event.preventDefault();
+  //unique Id check function
+
+  // pushing data to localStorage
+
+  let obj = {
+    description: formData.name.value,
+    image: formData.image.value,
+    price: formData.price.value,
+    type: formData.type.vakue,
+    id: formData.productid.value,
+  };
+  if (checkID(obj.id) === true) {
+    reviewItems++;
+
+    productLS.push(obj);
+    localStorage.setItem("New-Product", JSON.stringify(productLS));
+    localStorage.setItem("added-Product", JSON.stringify(productLS));
+    localStorage.setItem("review", JSON.stringify(reviewItems));
+    alert("Product added sucessfully");
+  } else {
+    alert("Product ID already exists ! use differnt Product ID");
+  }
+}
+
+// unique Id check function
+
+function checkID(el) {
+  let flag = true;
+  for (let i = 0; i < productLS.length; i++) {
+    if (el == productLS[i].id) {
+      flag = false;
+      break;
+    }
+  }
+
+  if (flag == true) {
+    return true;
+  } else {
+    return false;
+  }
+}

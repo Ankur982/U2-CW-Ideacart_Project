808594c574935cb23c20054b13a80ea94c36c18b
akshay-puranik <akshay.social@gmail.com>
2022-06-19 15:37:20 +0530

    updated cart,home,signin JS

--- signin.js
+++ signin.js
@@ -9,23 +9,33 @@ function mySignIn(event) {
   }
 
   if (data.userEmail == "admin@gmail.com" && data.userPassword == "admin") {
-    window.location.href = "https://www.google.com";
-  } else if (checkSignin(data.userEmail, data.userPassword) == true) {
-    alert("sign in successful")
-    window.location.href = "home_page.html";
-    localStorage.setItem("signinData", JSON.stringify(data));
-  } else {
-    alert("Invalid email or password")
+    alert("Welcome Admin !")
+    window.location.href = "www.google.com";
+    return;
   }
-
-  function checkSignin(email, password) {
-    let filter = users.filter(function(ele) {
-      return ele.email == email && ele.password == password
-    })
-    if (filter.length > 0) {
-      return true;
-    } else {
-      return false
+  else {
+    function checkSignin(email, password) {
+      let filter = users.filter(function(ele) {
+        return ele.email == email && ele.password == password
+      })
+      if (filter.length > 0) {
+        return filter;
+      } else {
+        return false
+      }
+    }
+    let currentUser = checkSignin(data.userEmail, data.userPassword);
+    if (currentUser == false) {
+      alert("User Not Found");
+    }
+  
+    else {
+      localStorage.setItem("signinData", JSON.stringify(currentUser));
+      alert("SignIn Success!!");
+      window.location.href = "home_page.html";
     }
   }
+  
+
 }
+
